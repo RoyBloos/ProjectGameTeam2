c4cc11cdea1c157d383f67d8cca750fac32ca96a
0801613 <0801613@hr.nl>
2016-12-09 00:05:36 +0100

    Minigame3 create actors

--- WorldMinigame3.java
+++ WorldMinigame3.java
@@ -12,5 +12,6 @@ public class WorldMinigame3 extends World
     public WorldMinigame3(World parentWorld, int gameHeight, int gameWidth)
     {    
         super(gameHeight, gameWidth, 1); 
+        addObject(new ConveyorBelt(), 600, 280);
     }
 }
